4f154c9df61fa514ba065b8c75db701c6c69a514
MariyaMalanova <79398183+MariyaMalanova@users.noreply.github.com>
2022-12-16 17:05:09 +0800

    Add files via upload

---
+++ dinoo.py
@@ -0,0 +1,29 @@
+import numpy as np
+from mss import mss 
+import cv2
+import pyautogui
+
+num = 1
+cod = {'top': 385, 'left':155, 'width':70, 'height':60}
+val = 17290
+while True:
+    cam = mss().grab(cod)
+    cam = np.array(cam)
+    cv2.imshow('Image',cam)
+    cactus = cam[28,:,0]
+    bird = cam[1,:,0]
+    cactus_sum = np.sum(cactus)
+    bird_sum = np.sum(bird)
+
+    if cactus_sum < val:
+        pyautogui.press('up')
+    if bird_sum < val:
+        pyautogui.press('down')
+        num = 1
+    if bird_sum == val and num ==1 :
+        pyautogui.keyUp('down')
+        num = 0
+
+    if cv2.waitKey(25) & 0xFF == ord("q"):
+        cv2.destroyAllWindows()
+        break
